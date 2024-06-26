4e4b72a6f0c4f51bbcc997a36600f121a2a33e74
Okasha Habib <160734391+okashabib-oh@users.noreply.github.com>
2024-06-27 04:29:21 +0500

    changes

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-const cards = document.querySelectorAll('#cards')
-console.log(cards);
+const columns = document.querySelectorAll('.column');
 
-// for(let i = 0; i < cards.length; i++){
-//     console.log(cards[i]);
-// }
+const addTask = (event) => {
+    event.preventDefault();
 
+    const currentForm = event.target; // current form element
+    const value = currentForm.elements[0].value; // value written in form's input
+    const parent = currentForm.parentElement; // parent of form i.e. li
+    const task = createTask(value); // li to be added
+
+    parent.insertBefore(task, currentForm); // adding new task before the form
+
+    currentForm.reset(); // clearing form
+};
+
+const createTask = (value) => {
+    const task = document.createElement('li');
+    task.className = "mb-3";
+    const taskSpan = document.createElement('span');
+    taskSpan.className = "flex-1 whitespace-nowrap flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white";
+    taskSpan.setAttribute("draggable", "true");
+    const text = document.createTextNode(value);
+    taskSpan.appendChild(text);
+    task.appendChild(taskSpan);
+    return task;
+};
+
+columns.forEach(column => {
+    const form = column.querySelector('.todo-form');
+    form.addEventListener('submit', addTask);
+});
